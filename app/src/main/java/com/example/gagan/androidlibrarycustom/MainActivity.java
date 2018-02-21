719c3d6786ce56c990a4de23758b04f5bfd363f9
Gagan <gagan.s.patil18@gmail.com>
2018-02-21 14:48:18 +0530

    new req

--- app/src/main/java/com/example/gagan/androidlibrarycustom/MainActivity.java
+++ app/src/main/java/com/example/gagan/androidlibrarycustom/MainActivity.java
@@ -3,7 +3,8 @@ package com.example.gagan.androidlibrarycustom;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.library.CustomThrmoMeter;
+import com.example.mylibrary.CustomThrmoMeter;
+
 
 public class MainActivity extends AppCompatActivity {
 
--- mylibrary/src/main/java/com/example/mylibrary/CustomThrmoMeter.java
+++ mylibrary/src/main/java/com/example/mylibrary/CustomThrmoMeter.java
@@ -1,11 +1,10 @@
-package com.example.library;
+package com.example.mylibrary;
 
 /**
- * Created by Gagan on 2/20/2018.
+ * Created by Gagan on 2/21/2018.
  */
 
 public class CustomThrmoMeter {
-    //To convert temperatures in degrees Fahrenheit to Celsius, subtract 32 and multiply by .5556 (or 5/9).
     public static float ToDegreeCelsius(float Fahrenheit) {
         float celsius = 5 * (Fahrenheit - 32) / 9;
         return celsius;
@@ -15,5 +14,4 @@ public class CustomThrmoMeter {
         float farenheit = Celsius * (9 / 5) + 32;
         return farenheit;
     }
-
 }
